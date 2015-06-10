c6cc37c142dc07c0a14dba9698b421569cb8a3e0
David <piense@gmail.com>
2015-06-09 23:52:11 -0700

    php Date fix

--- scripts/getDate.php
+++ scripts/getDate.php
@@ -1,7 +1,11 @@
 <?php
 require 'CCBEventParser.php';
 
-$day = $_GET["Year"]."-".$_GET["Month"]."-".$_GET["Day"];
+$date = $_GET["Day"];
+if(strlen($date) == 1)
+	$date = "0".$date;
+
+$day = $_GET["Year"]."-".$_GET["Month"]."-".$date;
 
 $startTime = date_create_from_format("Y-m-d H:i:s",$day."00:00:00");
 $endTime = date_create_from_format("Y-m-d H:i:s",$day."23:59:59");
