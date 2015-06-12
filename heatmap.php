9961d7805bae2cd3eb53a35279ce150fcf230790
Jason Tucker <jason@tucker.pro>
2015-06-11 21:23:23 -0700

    Added "minimal-ui" support for iPhone and iPad
    	<meta name="viewport" content="width=device-width,initial-scale=1.0, minimal-ui">

--- heatmap.php
+++ heatmap.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0, minimal-ui">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
 	<link rel="stylesheet" href="style.css">
--- wifi.php
+++ wifi.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0, minimal-ui">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/bootstrap-responsive.css" rel="stylesheet">
 	<link rel="stylesheet" href="style.css">
